e37c3c4508f8139aedc4e7f2867fd4696b261c6b
Xerxes Anthony <xerxesanthony25@gmail.com>
2022-07-12 11:00:26 +0800

    Adding of model files

---
+++ midterm_exam/lib/models/cart.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+class Product{
+
+int id;
+String title;
+int price;
+String category;
+String  description;
+ String  image;
+
+ Product({
+  required this.id;
+  required this.title;
+  required this.price;
+  this.category;
+  this.description;
+  this.image;
+
+ });
+
+}
--- midterm_exam/lib/services/api_service.dart
+++ midterm_exam/lib/services/api_service.dart
@@ -1,3 +1,4 @@
 class ApiService {
   static const String baseUrl = 'https://fakestoreapi.com';
+  
 }
