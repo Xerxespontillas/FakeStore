fae1167e140a0ebceb7e2ab1489aaa41bba8acdb
Xerxes Anthony <xerxesanthony25@gmail.com>
2022-07-12 20:04:46 +0800

    update in integrating user log in

--- midterm_exam/lib/services/api_service.dart
+++ midterm_exam/lib/services/api_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import '../models/api_response.dart';
 import '../models/product.dart';
 import 'package:fake_store/models/login.dart';
@@ -8,38 +7,30 @@ import 'package:http/http.dart' as http;
 class ApiService {
   static const baseUrl = 'https://fakestoreapi.com/';
 
-  static const headers = {
-    'token': "eyJhbGciOiJIUzI1NiIsInR",
-    'Content-Type': 'application/json'
-  };
-
  Future<dynamic> login(String username, String password) async{
-    return http.get(Uri.parse('$baseUrl/users1'), headers: headers).then((data){
-      if(data.statusCode==200){
+  final Auth = Login(username: username, password: password);
+    return http
+        .post(Uri.parse('$baseUrl/auth/login'), body: Auth.toJson())
+        .then((data) {
+      if (data.statusCode == 200) {
         final jsonData = json.decode(data.body);
         return jsonData;
       }
-    return APIResponse<Login>(error: true, errorMessage: 'An error occurred');
-    }).catchError((_) =>
-            APIResponse<Login>(error: true, errorMessage: 'Error occurred'));
+    }).catchError((err) => print(err));
 }
 
-
-
- /* Future<APIResponse<List<Product>>> getAllCategories() {
-    return http.get(Uri.parse('$baseUrl/notes'), headers: headers).then((data) {
+ Future<List<Product>> getAllProducts() async {
+    return http.get(Uri.parse('$baseUrl/products')).then((data) {
+      final products = <Product>[];
       if (data.statusCode == 200) {
         final jsonData = json.decode(data.body);
-        final products = <Product>[];
-        for (var item in jsonData) {
-          products.add(Product.fromJson(item));
+
+        for (var product in jsonData) {
+          products.add(Product.fromJson(product));
         }
-        return APIResponse<List<Product>>(data: products);
       }
-      return APIResponse<List<Product>>(
-          error: true, errorMessage: 'An error occurred');
-    }).catchError((_) => APIResponse<List<Product>>(
-        error: true, errorMessage: 'Error occurred'));
-  }*/
+      return products;
+    }).catchError((err) => print(err));
+  }
 
 }
